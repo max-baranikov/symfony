bcb91d3a7f06fa39608d9f420f399ec76e0ff134
Max Baranikov <max.baranikov@gmail.com>
2019-09-02 11:04:39 +0300

    Redefine Book's findAll method so it now order and cache the result

--- src/Repository/BookRepository.php
+++ src/Repository/BookRepository.php
@@ -19,32 +19,42 @@ class BookRepository extends ServiceEntityRepository
         parent::__construct($registry, Book::class);
     }
 
+    public function findAll()
+    {
+        return $this->createQueryBuilder('b')
+            ->orderBy('b.last_read', 'ASC')     // order by last_read field
+            ->getQuery()
+            ->useResultCache(true)              // cache result of this query
+            ->setResultCacheLifetime(86400)     // cache life time is 24 hours (60*60*24)
+            ->getResult()
+        ;
+    }
     // /**
     //  * @return Book[] Returns an array of Book objects
     //  */
     /*
     public function findByExampleField($value)
     {
-        return $this->createQueryBuilder('b')
-            ->andWhere('b.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('b.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
+    return $this->createQueryBuilder('b')
+    ->andWhere('b.exampleField = :val')
+    ->setParameter('val', $value)
+    ->orderBy('b.id', 'ASC')
+    ->setMaxResults(10)
+    ->getQuery()
+    ->getResult()
+    ;
     }
-    */
+     */
 
     /*
-    public function findOneBySomeField($value): ?Book
-    {
-        return $this->createQueryBuilder('b')
-            ->andWhere('b.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
+public function findOneBySomeField($value): ?Book
+{
+return $this->createQueryBuilder('b')
+->andWhere('b.exampleField = :val')
+->setParameter('val', $value)
+->getQuery()
+->getOneOrNullResult()
+;
+}
+ */
 }
