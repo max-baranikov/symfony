c66295dbf2a5992d3a0d18867a7f0c78baaf364c
Max Baranikov <max.baranikov@gmail.com>
2019-09-14 20:01:29 +0300

    Add Font Awesome

--- assets/js/app.js
+++ assets/js/app.js
@@ -8,6 +8,8 @@
 // any CSS you require will output into a single css file (app.css in this case)
 require('../css/app.scss');
 require('bootstrap');
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
 
 // require jequery stuff
 const $ = require('jquery');
